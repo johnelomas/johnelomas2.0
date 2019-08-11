7a29bcd1cd763e97186e3468b09332e9f745c44b
johnelomas <john@jelomas.com>
2019-08-10 22:35:03 -0400

    Initial Commit - Template
    Template.html is a template to use when creating actual site pages.
    
    StickyFooter.css and StickyFooterHTML5.js force a sticky footer - footer always is the last thing on the page.
    
    Custom.css allows page customizations including overriding Bootstrap settings.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # johnelomas2.0
  My personal website
+ 
+ A complete rewrite of my personal website using HTML, CSS, Javascript, Bootstrap 4.
---
+++ scripts/stickyfooter_html5.js
@@ -0,0 +1,27 @@
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('nav');
+/*
+     FILE ARCHIVED ON 20:37:30 Jun 29, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:13:09 Jul 30, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 77.335 (3)
+  esindex: 0.007
+  captures_list: 96.79
+  CDXLines.iter: 11.277 (3)
+  PetaboxLoader3.datanode: 42.304 (4)
+  exclusion.robots: 0.159
+  exclusion.robots.policy: 0.149
+  RedisCDXSource: 0.638
+  PetaboxLoader3.resolve: 91.753 (3)
+  load_resource: 62.424
+*/
